e31aa5b97af6d0a61037f0b859eae104a3e87605
Maksym Sobieszczuk <maksym.sob@gmail.com>
2024-12-22 17:55:04 +0100

    first commit

---
+++ 02_number_guessing_game.py
@@ -0,0 +1,38 @@
+import random
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a number larger than 0 next time.")
+        quit()
+else:
+    print("Please type a number next time.")
+    quit()
+
+random_number = random.randint(0, top_of_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input(f"Make a guess, please type a number between 0 and {top_of_range}: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number!")
+    else:
+        print("You were below the number!")
+
+if guesses == 1:
+    print(f"You got it in {guesses} guess.")
+else:
+    print(f"You got it in {guesses} guesses.")
---
+++ README.md
@@ -0,0 +1,3 @@
+# 9 HOURS of Python Projects - From Beginner to Advanced - Tech With Tim
+
+All the files in this repository were created based on this YouTube tutorial: 9 HOURS of Python Projects - From Beginner to Advanced, by Tech With Tim.
